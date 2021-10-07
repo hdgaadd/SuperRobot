57816e7aa035b2dd8f7d7ac6d72dc792efa4514c
CodeChen <53108370+hdgaadd@users.noreply.github.com>
2021-10-07 11:58:13 +0800

    test

---
+++ SuperRobot/src/main/java/communicateRobot/Test.java
@@ -0,0 +1,15 @@
+package communicateRobot;
+
+public class Test {
+    public static void main(String[] args) {
+        /*int[] res=new int[6];
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i:res
+             ) {
+            stringBuilder.append(i+"");
+        }
+        System.out.println(stringBuilder.toString());
+
+        ListenMusic.main(new String[]{"1"});*/
+    }
+}
